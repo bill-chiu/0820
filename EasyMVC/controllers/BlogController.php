df68e109b8bfbfb74e992a41dd9e9520370b911a
CS01183 <68837493+bill-chiu@users.noreply.github.com>
2020-08-20 12:36:10 +0800

    REST API

--- EasyMVC/controllers/BlogController.php
+++ EasyMVC/controllers/BlogController.php
@@ -1,16 +1,19 @@
 <?php
 
-class BlogController extends Controller {
-    
-    function index() {
+class BlogController extends Controller
+{
+
+    function index()
+    {
         echo "home page of HomeController";
     }
-    
-    function list($name) {
-        echo $name;
-    }
-    
-    
-}
 
-?>
+
+    function list($name)
+    {
+        $user = $this->model("User");
+        $user->name = $name;
+        $this->view("Blog/blog", $user);
+        //echo "Hello! $user->name"; 
+    }
+}
